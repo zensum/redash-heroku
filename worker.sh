8d428620cd180fe9cc5f16abb46cadd229c2ed35
Tobias Jeppesen <github@tobiasjeppesen.com>
2021-01-07 19:26:35 +0100

    Worker fixes

--- worker.sh
+++ worker.sh
@@ -2,12 +2,19 @@
 exec /app/bin/docker-entrypoint worker &
 exec /app/bin/docker-entrypoint scheduler &
 
+ps aux
+echo $?
+
 while sleep 60; do
   ps aux |grep '/app/bin/docker-entrypoint worker' |grep -q -v grep
   PROCESS_1_STATUS=$?
+  if [ $PROCESS_1_STATUS -ne 0 ]; then
+    echo "Redash worker died, killing pod"
+    exit 1
   ps aux |grep '/app/bin/docker-entrypoint scheduler' |grep -q -v grep
   PROCESS_2_STATUS=$?
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+  if [ $PROCESS_2_STATUS -ne 0 ]; then
+    echo "Redash scheduler died, killing pod"
     exit 1
   fi
 done
